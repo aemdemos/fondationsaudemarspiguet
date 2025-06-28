3174fdae49944392d181eb5f04c95faaab8dd892
Meet Kumar Jain <meetkjain83@gmail.com>
2025-06-29 03:54:07 +0530

    Buildingt the logic

--- blocks/articleintro/articleintro.js
+++ blocks/articleintro/articleintro.js
@@ -1,25 +1,26 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+// import { createOptimizedPicture } from '../../scripts/aem.js';
 // import { moveInstrumentation } from '../../scripts/scripts.js';
 import { div } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   const newsHeader = div(
-    { class: 'news-bloc-header' },
-    div({ class: 'page_hero_image' }),
-    div({ class: 'news_detail_big_header' }),
-    div({ class: 'page_hero_image_seconde' }),
+    { class: 'news-block-header' },
+    div({ class: 'page-hero-image' }, div({ class: 'page-hero-image-middle' })),
+    div({ class: 'news-detail-big-header' }),
+    div({ class: 'page-hero-image-seconde' }, div({ class: 'page-hero-image-seconde-middle' })),
   );
   [...block.children].forEach((row) => {
     row.querySelectorAll('img').forEach((img, i) => {
-      // if is 0, add the first image as background to div of class: 'page_hero_image'
+      // if is 0, add the first image as background to div of class: 'page-hero-image'
       if (i === 0) {
-        newsHeader.querySelector('.page_hero_image').style.backgroundImage = `url(${img.src})`;
-        console.log(newsHeader.querySelector('.page_hero_image'));
+        newsHeader.querySelector('.page-hero-image').style.backgroundImage = `url(${img.src})`;
+        console.log(newsHeader.querySelector('.page-hero-image'));
       } else if (i === 1) {
-        newsHeader.querySelector('.page_hero_image').append(createOptimizedPicture(img.src));
+        // newsHeader.querySelector('.page-hero-image').append(createOptimizedPicture(img.src));
+        newsHeader.querySelector('.page-hero-image-middle').style.backgroundImage = `url(${img.src})`;
       } else if (i === 2) {
-        // if is 1, add the second image as background to div of class: 'page_hero_image_seconde'
-        newsHeader.querySelector('.page_hero_image_seconde').style.backgroundImage = `url(${img.src})`;
+        // if is 1, add the second image as background to div of class: 'page-hero-image-seconde'
+        newsHeader.querySelector('.page-hero-image-seconde-middle').style.backgroundImage = `url(${img.src})`;
       }
     });
   });
