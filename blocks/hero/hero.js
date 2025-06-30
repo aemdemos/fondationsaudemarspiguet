bea4dc8d4613d437e2de82753cde9b41c3011d26
Neha Kaushik <nekaushi@Mac.lan>
2025-06-30 23:15:44 +1000

    manual styling changes to hero video block

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,5 +1,25 @@
 export default function decorate(block) {
-  if (!block.querySelector(':scope > div:first-child picture')) {
-    block.classList.add('no-image');
+  // add embedded video into hero block
+  if (block.classList.contains('video')) {
+    const heroVideoPara = block.querySelector('div:first-of-type > div > p.button-container');
+    const heroVideoLink = block.querySelector('div:first-of-type > div >  p.button-container > a');
+    const videoURL = heroVideoLink.href;
+    const heroVideoWrapper = document.createElement('video');
+    const videoSource = document.createElement('source');
+    videoSource.setAttribute('src', videoURL);
+    videoSource.setAttribute('type', 'video/mp4');
+    heroVideoWrapper.setAttribute('title', videoURL);
+    heroVideoWrapper.setAttribute('autoplay', '');
+    heroVideoWrapper.setAttribute('muted', '');
+    heroVideoWrapper.setAttribute('loop', '');
+    heroVideoWrapper.className = 'video-cover';
+    heroVideoWrapper.append(videoSource);
+    heroVideoPara.replaceWith(heroVideoWrapper);
+
+    const socialCRDiv = block.querySelector('div:nth-of-type(2) > div:first-of-type');
+    socialCRDiv.className = 'social-cr-wrapper';
+
+    const legendDiv = block.querySelector('div:nth-of-type(2) > div:nth-of-type(2)');
+    legendDiv.className = 'slide-legend';
   }
 }
