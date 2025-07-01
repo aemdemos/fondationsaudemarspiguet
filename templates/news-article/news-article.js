0a02a94b307a2f65326bd89273d7fb35df893fbb
Meet Kumar Jain <meetkjain83@gmail.com>
2025-07-01 16:36:24 +0530

    Enabling same behavior for all pages

--- templates/news-article/news-article.js
+++ templates/news-article/news-article.js
@@ -1,4 +1,4 @@
-export function enableAnimationOnScroll() {
+export default function enableAnimationOnScroll() {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
@@ -11,29 +11,4 @@ export function enableAnimationOnScroll() {
 
   const elements = document.querySelectorAll('.animate-on-scroll');
   elements.forEach((el) => observer.observe(el));
-}
-
-function setMainHeightVar(headerEle, doc) {
-  const headerHeight = headerEle.offsetHeight;
-  const mainEle = doc.querySelector('main');
-  if (mainEle) {
-    mainEle.style.marginTop = `${headerHeight}px`;
-  }
-}
-
-function waitForHeaderHeight(doc) {
-  const headerEle = doc.querySelector('header .nav-wrapper');
-
-  if (headerEle) {
-    setMainHeightVar(headerEle, doc); // Initial call
-    window.addEventListener('resize', () => {
-      setMainHeightVar(headerEle, doc); // On resize
-    }); // Recalculate on resize
-  } else {
-    setTimeout(() => waitForHeaderHeight(doc), 100); // Retry if header not yet in DOM
-  }
-}
-
-export default async function decorate(doc) {
-  waitForHeaderHeight(doc);
 }
