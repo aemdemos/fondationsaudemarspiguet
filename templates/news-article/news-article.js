507ee6fc5ccb063b978b35780525a701f9ae7740
Neha Kaushik <nekaushi@Mac.lan>
2025-07-10 22:49:25 +1000

    additional styling fixes to news section

--- templates/news-article/news-article.js
+++ templates/news-article/news-article.js
@@ -1,4 +1,4 @@
-import { applyFadeUpAnimation } from '../../scripts/utils.js';
+// import { applyFadeUpAnimation } from '../../scripts/utils.js';
 
 export function enableAnimationOnScroll() {
   const observer = new IntersectionObserver((entries) => {
@@ -16,21 +16,35 @@ export function enableAnimationOnScroll() {
 }
 
 export default function decorate(doc) {
-  // add clear div between text and image paragraphs to avoid image floating to left
-  const imagePara = doc.querySelector('.section.white-lilac-bg .default-content-wrapper p:last-of-type');
+  // Move text content and image content to be encased by different divs
+  const whiteLilacSection = doc.querySelector('.section.white-lilac-bg');
+  const allParas = doc.querySelectorAll('.section.white-lilac-bg div > p');
+  const defaultDiv = doc.querySelector('.section.white-lilac-bg div.default-content-wrapper');
+  const ImageDiv = document.createElement('div');
+  ImageDiv.className = 'image-wrapper';
   const clearDiv = doc.createElement('div');
   clearDiv.className = 'clear';
-  if (imagePara) {
-    imagePara.insertAdjacentElement('beforebegin', clearDiv);
-  }
 
-  // add clear div to after the default content wrapper to avoid layout issues with next section
-  const defaultDiv = doc.querySelector('.section.white-lilac-bg .default-content-wrapper');
-  if (defaultDiv) {
-    defaultDiv.appendChild(clearDiv.cloneNode());
+  if (allParas && defaultDiv && whiteLilacSection) {
+    defaultDiv.innerHTML = ''; // Clear the default content wrapper
+    let numTextPara = 0;
+    let numImagePara = 0;
+    allParas.forEach((p) => {
+      if (!p.querySelector('picture')) {
+        defaultDiv.appendChild(p);
+        numTextPara += 1;
+      } else if (p.querySelector('picture')) {
+        ImageDiv.appendChild(p);
+        numImagePara += 1;
+      }
+    });
+    if (numTextPara > 0) {
+      whiteLilacSection.appendChild(defaultDiv);
+      whiteLilacSection.appendChild(clearDiv);
+    }
+    if (numImagePara > 0) {
+      whiteLilacSection.appendChild(ImageDiv);
+      whiteLilacSection.appendChild(clearDiv.cloneNode());
+    }
   }
-
-  // apply fade out animation to news detail section
-  const pictureEl = doc.querySelector('.section.white-lilac-bg .default-content-wrapper p:last-of-type picture');
-  applyFadeUpAnimation(pictureEl, imagePara);
 }
