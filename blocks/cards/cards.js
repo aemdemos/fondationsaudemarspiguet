c7c902483d3f7918a5c392917889e9bbe2c34a73
Rusmeen Khan <rusmeenk@Rusmeens-MacBook-Pro.local>
2025-07-18 18:00:42 +0530

    fixed cards(listing)

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,5 +1,8 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
+import {
+  div, h2, a, ul, li,
+} from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   /* change to ul, li */
@@ -21,4 +24,32 @@ export default function decorate(block) {
   });
   block.textContent = '';
   block.append(ul);
+  if(block.classList.contains('listing')) {
+    const cardsList = block.querySelectorAll('ul li');
+    cardsList.forEach((listItem) => {
+      const cardImage = listItem.querySelector('.cards-card-image');
+      const cardBody = listItem.querySelector('.cards-card-body');
+      const cardImageWrapper = div({class: 'cards-image-container'});
+      const cardImageLogo = div({class: 'cards-image-logo'});
+      cardImageWrapper.append(cardImage, cardImageLogo);
+      const link = cardBody.querySelector('p a');
+      if(link) {
+        cardBody.innerHTML = '';
+        const heading = h2(link.textContent);
+        link.innerHTML = '';
+        link.classList.remove('button', 'primary');
+        link.append(cardImageWrapper, heading);
+        cardBody.append(link);
+      }
+      else {
+        const titlePara = cardBody.querySelector('p strong');
+        if(titlePara) {
+          const heading = h2(titlePara.textContent);
+          cardBody.innerHTML = '';
+          cardBody.classList.add('no-link');
+          cardBody.append(cardImageWrapper, heading);
+        }
+      }
+    })
+  }
 }
