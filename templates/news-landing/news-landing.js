f447f77fa5376d7f90c7dcef69f4648c79e6534a
Rusmeen Khan <rusmeenk@Rusmeens-MacBook-Pro.local>
2025-07-28 10:35:50 +0530

    fixed js

--- templates/news-landing/news-landing.js
+++ templates/news-landing/news-landing.js
@@ -1,5 +1,5 @@
 import {
-  div, section, a,
+  div, section,input,span,a
 } from '../../scripts/dom-helpers.js';
 import { getLanguage } from '../../scripts/scripts.js';
 import ffetch from '../../scripts/ffetch.js';
@@ -19,11 +19,17 @@ export default async function decorate(doc) {
   const $section = section();
   const $filterContainer = div({ class: 'filter-container' });
 
-  const $newsListingLeft = div({ class: 'news-listing-container-left' });
+  // const $newsListingLeft = div({ class: 'news-listing-container-left' });
   const $newsListingRight = div({ class: 'news-listing-container-right' });
-
   const $filterTop = a({ class: 'filter-top-btn' });
   const $fitlerBottom = a({ class: 'filter-bottom-btn' });
+
+  $newsListingRight.append($filterTop, $fitlerBottom);
+  const $newsListingLeft = div(
+      { class: 'news-listing-container-left' },
+      div({ class: 'category-section' }, input({ class: 'category-input', id: 'filtercategories-selectized' , placeholder: 'Category', type: 'text', autofill:'no' })), span({ class: 'filter-separator' }, ' | '), a({ class: 'view-all', href: '#', id: 'view-all' }, 'View All'),
+      span({ class: 'filter-separator' }, ' | '),
+      div({ class: 'search-section' }, input({ class: 'search-input', id: 'filtersearch', placeholder: 'Search...', type: 'text', minlength: '2', size: '10' })), a({ class: 'btn-search-clear', href: '#'}));
 
   $newsListingRight.append($filterTop, $fitlerBottom);
 
