fc1f37387b4bcc2a1f41ce627cd7669070a6b0ba
Neha Kaushik <neha.k06@gmail.com>
2025-07-30 14:02:33 +1000

    part 1 of manual changes to the category ribbon

--- templates/projects-landing/projects-landing.js
+++ templates/projects-landing/projects-landing.js
@@ -1,36 +1,44 @@
 import {
-  div, section,
+  div, section, input, span, a,
 } from '../../scripts/dom-helpers.js';
-import { getLanguage } from '../../scripts/scripts.js';
-import ffetch from '../../scripts/ffetch.js';
-import {
-  fetchPlaceholders,
-} from '../../scripts/aem.js';
-
-async function getProjectsdata() {
-  const rawProjects = await ffetch(`/${getLanguage()}/${getLanguage() === 'en' ? 'fondation-pour-les-arbres-projects' : 'fondation-pour-les-arbres-nos-projets'}/projects-index.json`)
-    .chunks(1000)
-    .all();
-  return rawProjects;
-}
 
 export default async function decorate(doc) {
   const $main = doc.querySelector('main');
   const $section = section();
   const $filterContainer = div({ class: 'filter-container' });
 
-  const $projectsListingLeft = div({ class: 'projects-listing-container-left' });
+  // const $projectsListingLeft = div({ class: 'projects-listing-container-left' });
   const $projectsListingRight = div({ class: 'projects-listing-container-right' });
+  const $filterTop = a({ class: 'filter-top-btn' });
+  const $filterBottom = a({ class: 'filter-bottom-btn' });
+
+  $projectsListingRight.append($filterTop, $filterBottom);
+  const $projectsListingLeft = div(
+    { class: 'projects-listing-container-left' },
+    div(
+      { class: 'category-section' },
+      input(
+        {
+          class: 'category-input', id: 'filtercategories-selectized', placeholder: 'Category', type: 'text', autofill: 'no',
+        },
+      ),
+    ),
+    span({ class: 'filter-separator' }, ' | '),
+    a({ class: 'view-all', href: '#', id: 'view-all' }, 'View All'),
+    span({ class: 'filter-separator' }, ' | '),
+    div(
+      { class: 'search-section' },
+      input(
+        {
+          class: 'search-input', id: 'filtersearch', placeholder: 'Search...', type: 'text', minlength: '2', size: '10',
+        },
+      ),
+    ),
+    a({ class: 'btn-search-clear', href: '#' }),
+  );
 
   $filterContainer.append($projectsListingLeft, $projectsListingRight);
-
   const $projectsListing = div({ class: 'projects-listing' });
   $section.append($filterContainer, $projectsListing);
-  const placeholders = await fetchPlaceholders(`${getLanguage()}`);
-  console.log(placeholders);
-
-  const getProjects = await getProjectsdata();
-  console.log(getProjects);
-
   $main.append($section);
 }
