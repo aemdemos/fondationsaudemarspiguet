dfc4d2096bbb644f876d718c2a8bc5b62221141a
Neha Kaushik <neha.k06@gmail.com>
2025-07-30 16:22:35 +1000

    part 2 of manual changes to the category section

--- templates/projects-landing/projects-landing.js
+++ templates/projects-landing/projects-landing.js
@@ -1,36 +1,54 @@
 import {
   div, section, input, span, a,
 } from '../../scripts/dom-helpers.js';
+import {
+  fetchPlaceholders,
+} from '../../scripts/aem.js';
+import { getLanguage } from '../../scripts/scripts.js';
 
 export default async function decorate(doc) {
   const $main = doc.querySelector('main');
   const $section = section();
   const $filterContainer = div({ class: 'filter-container' });
 
-  // const $projectsListingLeft = div({ class: 'projects-listing-container-left' });
-  const $projectsListingRight = div({ class: 'projects-listing-container-right' });
-  const $filterTop = a({ class: 'filter-top-btn' });
-  const $filterBottom = a({ class: 'filter-bottom-btn' });
+  const placeholders = await fetchPlaceholders(`${getLanguage()}`);
+  const { projectsLandingCategoryFilter } = placeholders;
+  const { projectsLandingLocationFilter } = placeholders;
+  const { projectsLandingViewFilter } = placeholders;
+  const { projectsLandingSearchFilter } = placeholders;
 
-  $projectsListingRight.append($filterTop, $filterBottom);
+  const $projectsListingRight = div({ class: 'projects-listing-container-right' });
+  const $mapLink = a({ class: 'map-link-btn', href: '/en/fondation-pour-les-arbres-projects-map', id: 'map-link' }, 'See Map');
+
+  $projectsListingRight.append($mapLink);
+
   const $projectsListingLeft = div(
     { class: 'projects-listing-container-left' },
     div(
       { class: 'category-section' },
       input(
         {
-          class: 'category-input', id: 'filtercategories-selectized', placeholder: 'Category', type: 'text', autofill: 'no',
+          class: 'category-input', id: 'filtercategories-selectized', placeholder: projectsLandingCategoryFilter, type: 'text', autofill: 'no',
         },
       ),
     ),
     span({ class: 'filter-separator' }, ' | '),
-    a({ class: 'view-all', href: '#', id: 'view-all' }, 'View All'),
+    div(
+      { class: 'location-dropdown' },
+      input(
+        {
+          class: 'location-input', id: 'filterlocations-selectized', placeholder: projectsLandingLocationFilter, type: 'text', autofill: 'no',
+        },
+      ),
+    ),
+    span({ class: 'filter-separator' }, ' | '),
+    a({ class: 'view-all', href: '#', id: 'view-all' }, projectsLandingViewFilter),
     span({ class: 'filter-separator' }, ' | '),
     div(
       { class: 'search-section' },
       input(
         {
-          class: 'search-input', id: 'filtersearch', placeholder: 'Search...', type: 'text', minlength: '2', size: '10',
+          class: 'search-input', id: 'filtersearch', placeholder: projectsLandingSearchFilter, type: 'text', minlength: '2', size: '10',
         },
       ),
     ),
